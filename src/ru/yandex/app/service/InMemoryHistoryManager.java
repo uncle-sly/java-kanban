7392f9df008f2cc57559540ac1dbff11632fd996
uncle-sly <Unclesly@yandex.ru>
2024-05-21 00:53:18 +0300

    Sprint 6 Solution v1

--- src/ru/yandex/app/service/InMemoryHistoryManager.java
+++ src/ru/yandex/app/service/InMemoryHistoryManager.java
@@ -4,7 +4,6 @@ import ru.yandex.app.model.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
